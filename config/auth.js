63507ec8620cba78bb211d7e8fe8ff91377f1d36
Michael Koval <mkoval3@gatech.edu>
2017-07-19 00:14:23 -0400

    testing different environment

--- config/auth.js
+++ config/auth.js
@@ -1,24 +1,49 @@
 
 
 // expose our config directly to our application using module.exports
-module.exports = {
+var config ={
 
-    'facebookAuth' : {
-        'clientID'      : '336554796778464', // your App ID
-        'clientSecret'  : '5c79a7bfd7a259b0b2a0b030e18d8f14', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    production:{
+
+        'facebookAuth' : {
+            'clientID'      : '336554796778464', // your App ID
+            'clientSecret'  : '5c79a7bfd7a259b0b2a0b030e18d8f14', // your App Secret
+            'callbackURL'   : 'https://secure-harbor-24413.herokuapp.com/'
+        },
+
+        'twitterAuth' : {
+            'consumerKey'       : 'ioocTHWwGBOcQypgltHK61cel',
+            'consumerSecret'    : 'yBVQjM2w4o7g4pilFoF1JOONdv2RaA4u7gTjYeRg1SoN4wOwxD',
+            'callbackURL'       : 'https://secure-harbor-24413.herokuapp.com/'
+        },
+
+        'googleAuth' : {
+            'clientID'      : '597384415679-o0i0ko1qepdm92n5mqnia8ecavuft3mt.apps.googleusercontent.com',
+            'clientSecret'  : '4i-SSRBDiTup874bLhuSdLCD',
+            'callbackURL'   : 'https://secure-harbor-24413.herokuapp.com/'
+        }
     },
+    default:{
+        'facebookAuth' : {
+            'clientID'      : '336554796778464', // your App ID
+            'clientSecret'  : '5c79a7bfd7a259b0b2a0b030e18d8f14', // your App Secret
+            'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        },
 
-    'twitterAuth' : {
-        'consumerKey'       : 'ioocTHWwGBOcQypgltHK61cel',
-        'consumerSecret'    : 'yBVQjM2w4o7g4pilFoF1JOONdv2RaA4u7gTjYeRg1SoN4wOwxD',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
-    },
+        'twitterAuth' : {
+            'consumerKey'       : 'ioocTHWwGBOcQypgltHK61cel',
+            'consumerSecret'    : 'yBVQjM2w4o7g4pilFoF1JOONdv2RaA4u7gTjYeRg1SoN4wOwxD',
+            'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        },
 
-    'googleAuth' : {
-        'clientID'      : '597384415679-o0i0ko1qepdm92n5mqnia8ecavuft3mt.apps.googleusercontent.com',
-        'clientSecret'  : '4i-SSRBDiTup874bLhuSdLCD',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'googleAuth' : {
+            'clientID'      : '597384415679-o0i0ko1qepdm92n5mqnia8ecavuft3mt.apps.googleusercontent.com',
+            'clientSecret'  : '4i-SSRBDiTup874bLhuSdLCD',
+            'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        }
     }
 
 };
+exports.get = function get(env) {
+  return config[env] || config.default;
+}
