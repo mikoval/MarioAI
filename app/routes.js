3456cd450a63c7b8ef3a8eb7b231893c132e3965
Michael Koval <mkoval3@gatech.edu>
2017-07-19 00:26:20 -0400

    more noticable message

--- app/routes.js
+++ app/routes.js
@@ -96,7 +96,9 @@ module.exports = function(app, passport) {
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
+    console.log("____________________")
     console.log(req.isAuthenticated());
+    console.log("____________________")
     if (req.isAuthenticated())
         return next();
 
