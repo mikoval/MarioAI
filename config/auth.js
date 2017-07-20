9c336c78545203b5e13d255126b2fd8486939303
Michael Koval <mkoval3@gatech.edu>
2017-07-20 09:37:46 -0400

    oops, not using facebook callback

--- config/auth.js
+++ config/auth.js
@@ -8,19 +8,19 @@ var config ={
         'facebookAuth' : {
             'clientID'      : '312744345803447', // your App ID
             'clientSecret'  : 'f7d46350e79b5ab9254f7d696026de7d', // your App Secret
-            'callbackURL'   : 'https://secure-harbor-24413.herokuapp.com/'
+            'callbackURL'   : 'https://secure-harbor-24413.herokuapp.com/auth/facebook/callback'
         },
 
         'twitterAuth' : {
             'consumerKey'       : 'ioocTHWwGBOcQypgltHK61cel',
             'consumerSecret'    : 'yBVQjM2w4o7g4pilFoF1JOONdv2RaA4u7gTjYeRg1SoN4wOwxD',
-            'callbackURL'       : 'https://secure-harbor-24413.herokuapp.com/'
+            'callbackURL'       : 'https://secure-harbor-24413.herokuapp.com/auth/twitter/callback'
         },
 
         'googleAuth' : {
             'clientID'      : '597384415679-o0i0ko1qepdm92n5mqnia8ecavuft3mt.apps.googleusercontent.com',
             'clientSecret'  : '4i-SSRBDiTup874bLhuSdLCD',
-            'callbackURL'   : 'https://secure-harbor-24413.herokuapp.com/'
+            'callbackURL'   : 'https://secure-harbor-24413.herokuapp.com/auth/google/callback'
         }
     },
     default:{
